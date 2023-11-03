766c7d983096a4e6b33f178903c1814a30d0aec0
Darius Niminenn <root@dnim.dev>
2023-11-03 01:17:13 -0300

    make read_file case insensitive

--- src/common/file_loader.rs
+++ src/common/file_loader.rs
@@ -4,10 +4,43 @@ use tokio::fs::File;
 use tokio::io::{BufReader, AsyncReadExt};
 use serde_json;
 use tokio::io;
-use std::env;
+use std::{env, fs};
+
+/*pub async fn read_file(path: &Path) -> io::Result<String> {
+    let file = File::open(path).await?;
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents).await?;
+    Ok(contents)
+}*/
 
 pub async fn read_file(path: &Path) -> io::Result<String> {
-    let file = File::open(path).await?;
+    let file_name_lowercase = path
+        .file_name()
+        .ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, "File name not found"))?
+        .to_str()
+        .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "Invalid file name"))?
+        .to_lowercase();
+
+    let directory = path.parent().ok_or_else(|| {
+        io::Error::new(io::ErrorKind::NotFound, "Directory for the file not found")
+    })?;
+
+    let dir_entries = fs::read_dir(directory)?;
+
+    let case_insensitive_path = dir_entries
+        .filter_map(|entry| entry.ok())
+        .find(|entry| {
+            entry
+                .file_name()
+                .to_str()
+                .map(|s| s.to_lowercase() == file_name_lowercase)
+                .unwrap_or(false)
+        })
+        .ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, "File not found"))?
+        .path();
+
+    let file = File::open(case_insensitive_path).await?;
     let mut buf_reader = BufReader::new(file);
     let mut contents = String::new();
     buf_reader.read_to_string(&mut contents).await?;
