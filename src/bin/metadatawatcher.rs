f2eaaf8d3e9350cbe0f459d0f3c93b2b746dfb7b
Darius Niminenn <root@dnim.dev>
2023-11-04 09:40:20 -0300

    okay lol, better batching

--- src/bin/metadatawatcher.rs
+++ src/bin/metadatawatcher.rs
@@ -1,6 +1,6 @@
 use ignore::WalkBuilder;
 use std::collections::HashSet;
-use std::fs::File;
+use std::fs::{File, OpenOptions};
 use std::io::{BufRead, BufReader, Write};
 use std::path::Path;
 use std::process::Command;
@@ -8,16 +8,16 @@ use std::thread;
 use std::time::Duration;
 use dotenv::dotenv;
 use std::env;
+use std::error::Error;
 
 const PROCESSED_FILE: &str = "processed_files.txt";
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), Box<dyn Error>> {
     dotenv().ok();
     println!("Starting metadata watcher");
     let mut processed: HashSet<String> = HashSet::new();
     let processed_file_path = PROCESSED_FILE;
 
-    // Load previously processed files
     if Path::new(&processed_file_path).exists() {
         let file = File::open(&processed_file_path)?;
         let reader = BufReader::new(file);
@@ -28,9 +28,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 
+    let data_path = env::var("AFTERLIFE_PATH_METADATA")?;
+    let base_path = Path::new(&data_path);
+
     loop {
-        let data_path = env::var("AFTERLIFE_PATH_METADATA");
-        let walker = WalkBuilder::new(data_path.unwrap())
+        let mut new_files = Vec::new();
+        let walker = WalkBuilder::new(&base_path)
             .add_custom_ignore_filename(".ignore")
             .build();
 
@@ -38,31 +41,56 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             if let Ok(entry) = result {
                 if entry.file_type().map_or(false, |ft| ft.is_file()) {
                     let path = entry.path();
-                    if path.extension().and_then(|s| s.to_str()) == Some("json") {
-                        let file_name = path.file_name().unwrap().to_str().unwrap().to_string();
+                    if let Some("json") = path.extension().and_then(|s| s.to_str()) {
+                        let relative_path = path.strip_prefix(base_path)
+                            .unwrap_or(path)
+                            .with_extension("")
+                            .to_string_lossy()
+                            .into_owned();
 
-                        if !processed.contains(&file_name) {
-                            process_new_file(path)?;
-                            processed.insert(file_name.clone());
-
-                            // Append the processed file to the record
-                            let mut file = File::create(&processed_file_path)?;
-                            for id in &processed {
-                                writeln!(file, "{}", id)?;
-                            }
+                        if !processed.contains(&relative_path) {
+                            new_files.push(relative_path);
                         }
                     }
                 }
             }
         }
+
+        // Process new files if there are any and no script is running
+        if !new_files.is_empty() && !are_scripts_running()? {
+            process_files()?;
+
+            // Mark new files as processed
+            let mut file = OpenOptions::new()
+                .create(true)
+                .write(true)
+                .append(true)
+                .open(&processed_file_path)?;
+
+            for file_id in new_files {
+                writeln!(file, "{}", file_id)?;
+                processed.insert(file_id);
+            }
+        }
+
         println!("Done, sleeping for 60 seconds");
         thread::sleep(Duration::from_secs(60));
     }
 }
 
-fn process_new_file(path: &Path) -> Result<(), Box<dyn std::error::Error>> {
-    println!("Processing new file: {:?}", path);
+fn are_scripts_running() -> Result<bool, Box<dyn Error>> {
+    let scripts_regex = "metadatafetcher.py|cloudflarize.py|rarity_scorer.py";
 
+    let pgrep_output = Command::new("pgrep")
+        .arg("-f")
+        .arg(scripts_regex)
+        .output()?;
+
+    Ok(!pgrep_output.stdout.is_empty())
+}
+
+fn process_files() -> Result<(), Box<dyn Error>> {
+    // Execute the scripts
     let command_line = "\
         /home/jr/afterlife-awesome/venv/bin/python /home/jr/afterlife-awesome/metadatafetcher.py && \
         /home/jr/afterlife-awesome/venv/bin/python /home/jr/afterlife-awesome/cloudflarize.py && \
@@ -81,4 +109,3 @@ fn process_new_file(path: &Path) -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
-
